dc324824fa53d5318ac368b8b4f5be8b7f954f6e
Varga Balázs Ádám <112722847+VargaBalazsAdam@users.noreply.github.com>
2023-10-04 23:24:08 +0200

    basic

--- src/app/simulate/simulate.component.ts
+++ src/app/simulate/simulate.component.ts
@@ -1,10 +1,136 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-simulate',
   templateUrl: './simulate.component.html',
   styleUrls: ['./simulate.component.css']
 })
-export class SimulateComponent {
+export class SimulateComponent implements OnInit {
+  private cube1: any = {
+    id: "cube1",
+    mass: 0,
+    force: 0,
+    position: 0,
+    velocity: 0,
+    width: 0
+  };
+  private cube2: any = {
+    id: "cube2",
+    mass: 0,
+    force: 0,
+    position: 0,
+    velocity: 0,
+    width: 0
+  };
+  private animationFrameId: number | null = null;
+  ngOnInit(): void {
+  }
 
+  startSimulation(): void {
+    if (this.animationFrameId != null) {
+      this.stopSimulation();
+      return;
+    }
+    const xAttribute1 = (<HTMLInputElement>document.getElementById('cube1')).getAttribute('x');
+    if (xAttribute1 !== null) {
+      this.cube1.position = parseFloat(xAttribute1);
+    }
+    const xAttribute2 = (<HTMLInputElement>document.getElementById('cube2')).getAttribute('x');
+    if (xAttribute2 !== null) {
+      this.cube2.position = parseFloat(xAttribute2);
+    }
+    const wAttribute1 = (<HTMLInputElement>document.getElementById('cube1')).getAttribute('width');
+    if (wAttribute1 !== null) {
+      this.cube1.width = parseFloat(wAttribute1);
+    }
+    const wAttribute2 = (<HTMLInputElement>document.getElementById('cube2')).getAttribute('width');
+    if (wAttribute2 !== null) {
+      this.cube2.width = parseFloat(wAttribute2);
+    }
+    this.cube1.mass = parseFloat((<HTMLInputElement>document.getElementById('cube1-weight')).value);
+    this.cube1.force = parseFloat((<HTMLInputElement>document.getElementById('cube1-force')).value);
+    this.cube2.mass = parseFloat((<HTMLInputElement>document.getElementById('cube2-weight')).value);
+    this.cube2.force = parseFloat((<HTMLInputElement>document.getElementById('cube2-force')).value);
+
+    this.updateSimulation();
+  }
+
+  minVelocity = 0.09;
+
+  updateCube(cube: any): void {
+    console.log(cube)
+    const acceleration = cube.force / cube.mass;
+    if (cube.velocity >= 0) {
+      cube.velocity += acceleration;
+    } else {
+      cube.velocity -= acceleration;
+    }
+    cube.position += cube.velocity;
+    if (cube.position >= 380 - cube.width) {
+      cube.position = 380 - cube.width;
+      cube.velocity = -Math.abs(cube.velocity) * 0.8;
+      if (this.checkVelocityValidation(cube))
+          return;
+    }
+    if (cube.position <= 10) {
+      cube.position = 10;
+      cube.velocity = Math.abs(cube.velocity) * 0.8;
+      if (this.checkVelocityValidation(cube))
+        return;
+    }
+    
+  }
+
+  checkVelocityValidation(cube:any):boolean{
+    if (Math.abs(cube.velocity) < this.minVelocity && this.animationFrameId !== null) {
+      cube.velocity = 0;
+      return false;
+    }
+    return true;
+  }
+
+  updateCubeSVG(cube: any): void {
+    const svgElement = document.getElementById(cube.id);
+    if (svgElement) {
+      svgElement.setAttribute('x', cube.position);
+    }
+  }
+
+  updateSimulation(): void {
+    this.updateCube(this.cube1);
+    this.updateCube(this.cube2);
+    this.updateCubeSVG(this.cube1);
+    this.updateCubeSVG(this.cube2);
+    this.checkCollision();
+    if (this.cube1.velocity == 0 && this.cube2.velocity == 0)
+    {
+      this.stopSimulation();
+      return;
+    }
+    this.animationFrameId = requestAnimationFrame(() => {
+      this.updateSimulation();
+    });
+  }
+
+  checkCollision(): void {
+    const relativeVelocity = this.cube2.velocity - this.cube1.velocity;
+    if (this.cube2.position + this.cube2.width >= this.cube1.position && relativeVelocity > 0) {
+      const tempVelocity1 = this.cube1.velocity + (this.cube2.mass / (this.cube1.mass + this.cube2.mass)) * relativeVelocity;
+      const tempVelocity2 = this.cube2.velocity - (this.cube1.mass / (this.cube1.mass + this.cube2.mass)) * relativeVelocity;
+  
+      this.cube1.velocity = tempVelocity1;
+      this.cube2.velocity = tempVelocity2;
+  
+      if (this.checkVelocityValidation(this.cube1) || this.checkVelocityValidation(this.cube2))
+        return;
+    }
+  }
+  
+
+  stopSimulation(): void {
+    if (this.animationFrameId !== null) {
+      cancelAnimationFrame(this.animationFrameId);
+      this.animationFrameId = null;
+    }
+  }
 }
